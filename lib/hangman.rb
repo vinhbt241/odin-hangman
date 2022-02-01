cf4a7a18ee561cfa3054b1fdbb0a8272fdb61404
Bui The Vinh <vinhbuith32412000@gmail.com>
2022-02-01 22:59:25 +0700

    Add function to guess the word, check if word is valid

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -106,6 +106,51 @@ def draw_hangman(guess_count)
   end
 end
 
-dictionary = File.open("google-10000-english-no-swears.txt", "r") 
+def get_char()
+  message = "Type in character you want to guess: "
+  char = ""
+  until char.length == 1 && char.match?(/[a-z]/)
+    print message
+    message = "Invalid input, please try again: "
+    char = gets.chomp.downcase
+  end
 
-puts draw_hangman(7)
+  char
+end
+
+def guess_word(word, display_arr)
+  word_to_guess = word.split("")
+
+  char = get_char()
+
+  index_of_chars = []
+
+  word_to_guess.each_with_index do |c, index|
+    index_of_chars.push(index) if c == char
+  end
+
+  index_of_chars.each do |index|
+    display_arr[index] = char
+  end
+
+  display_arr
+end
+
+# Run game
+dictionary = File.open("google-10000-english-no-swears.txt", "r")
+
+word = select_random_word(dictionary)
+
+display_guess = Array.new(word.length, "_")
+
+while display_guess.any?("_")
+  puts display_guess.join(" ")
+  display_guess = guess_word(word, display_guess)
+end
+
+puts "Word completed: #{display_guess.join("")}"
+
+
+
+
+
