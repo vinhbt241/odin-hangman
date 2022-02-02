e5b20489ebcb1760cd0d554cdd0241a8af52d685
Bui The Vinh <vinhbuith32412000@gmail.com>
2022-02-02 11:24:20 +0700

    Player now can see hang man, character guessed, win/lose condition  and know the secret word once complete the game

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -4,7 +4,7 @@ def select_random_word(dictionary)
   word = ""
   until word.length >= 5 && word.length <= 12
     dictionary.rewind()
-    pos_read = rand 1...line_count
+    pos_read = rand 1..line_count
     pos_read.times { word =  dictionary.readline().strip() }
   end
 
@@ -18,7 +18,7 @@ def draw_hangman(guess_count)
       ┏━━┓
       ╿  ┃
          ┃
-         ┃ 
+         ┃
          ┃
          ┃
          ┃
@@ -29,7 +29,7 @@ def draw_hangman(guess_count)
       ┏━━┓
       ╿  ┃
       ○  ┃
-         ┃ 
+         ┃
          ┃
          ┃
          ┃
@@ -40,7 +40,7 @@ def draw_hangman(guess_count)
       ┏━━┓
       ╿  ┃
       ○  ┃
-     ╱   ┃ 
+     ╱   ┃
          ┃
          ┃
          ┃
@@ -51,7 +51,7 @@ def draw_hangman(guess_count)
       ┏━━┓
       ╿  ┃
       ○  ┃
-     ╱│  ┃ 
+     ╱│  ┃
          ┃
          ┃
          ┃
@@ -62,7 +62,7 @@ def draw_hangman(guess_count)
       ┏━━┓
       ╿  ┃
       ○  ┃
-     ╱│╲ ┃ 
+     ╱│╲ ┃
          ┃
          ┃
          ┃
@@ -73,7 +73,7 @@ def draw_hangman(guess_count)
       ┏━━┓
       ╿  ┃
       ○  ┃
-     ╱│╲ ┃ 
+     ╱│╲ ┃
       │  ┃
          ┃
          ┃
@@ -84,7 +84,7 @@ def draw_hangman(guess_count)
       ┏━━┓
       ╿  ┃
       ○  ┃
-     ╱│╲ ┃ 
+     ╱│╲ ┃
       │  ┃
      ╱   ┃
          ┃
@@ -95,7 +95,7 @@ def draw_hangman(guess_count)
       ┏━━┓
       ╿  ┃
       ○  ┃
-     ╱│╲ ┃ 
+     ╱│╲ ┃
       │  ┃
      ╱ ╲ ┃
          ┃
@@ -118,10 +118,17 @@ def get_char()
   char
 end
 
-def guess_word(word, display_arr)
+def guess_word(word, display_guess, chars_guessed)
+  display_arr = display_guess.dup()
+
   word_to_guess = word.split("")
 
   char = get_char()
+  while chars_guessed.any?(char)
+    puts "Character was already guessed, please try another character"
+    char = get_char
+  end
+  chars_guessed.push(char)
 
   index_of_chars = []
 
@@ -133,7 +140,19 @@ def guess_word(word, display_arr)
     display_arr[index] = char
   end
 
-  display_arr
+  return display_arr, chars_guessed
+end
+
+def still_have_guesses?(display_guess, wrong_guesses)
+  display_guess.any?("_") && wrong_guesses < 7
+end
+
+def center_multiline_string(string)
+  centered_string = string.lines.map do |line|
+    line.gsub!("\n", "") 
+    line.center(44) 
+  end
+  centered_string.join("\n")
 end
 
 # Run game
@@ -143,12 +162,37 @@ word = select_random_word(dictionary)
 
 display_guess = Array.new(word.length, "_")
 
-while display_guess.any?("_")
-  puts display_guess.join(" ")
-  display_guess = guess_word(word, display_guess)
+wrong_guesses = 0
+
+chars_guessed = []
+
+while still_have_guesses?(display_guess, wrong_guesses)
+  system 'clear'
+  puts "HANGMAN".center(50)
+  puts center_multiline_string(draw_hangman(wrong_guesses))
+  puts display_guess.join(" ").center(50)
+  puts "\n"
+  puts "Characters guessed: #{chars_guessed.join(" ")}"
+  player_guess, chars_guessed = guess_word(word, display_guess, chars_guessed)
+  wrong_guesses += 1 if player_guess == display_guess
+  display_guess = player_guess
+
 end
 
+system 'clear'
+puts "HANGMAN".center(50)
+puts center_multiline_string(draw_hangman(wrong_guesses))
+puts display_guess.join(" ").center(50)
+puts "\n"
+puts "Characters guessed: #{chars_guessed.join(" ")}"
+
+puts "Correct word is: #{word}"
 puts "Word completed: #{display_guess.join("")}"
+if word == display_guess.join("")
+  puts "CONGRATULATION, YOU WON!"
+else
+  puts "OOPS, YOU LOST, TRY AGAIN?"
+end
 
 
 
